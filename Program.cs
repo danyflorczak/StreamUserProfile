5f5a77b3e3299ae601a1ffa9dc721d8e3816f7e1
Daniel Florczak <danyflorczak@gmail.com>
2023-01-25 02:52:30 +0100

    creating request to list all games with given steam id key and api key

--- Program.cs
+++ Program.cs
@@ -1,20 +1,28 @@
 ﻿using System;
-using System.Net;
+using System.Net.Http;
 using Newtonsoft.Json;
 
 class SteamAPI {
     static void Main(string[] args) {
-        string steamId = "YOUR_STEAM_ID";
-        string apiKey = "YOUR_API_KEY";
-        string url = $"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={apiKey}&steamid={steamId}&format=json";
+        string steamId = "76561198209273776";
+        string apiKey = "EF5C8E2B08A10F7FF30BFBC4A595C2BB";
+        string url = $"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={apiKey}&include_appinfo=true&steamid={steamId}&format=json";
 
-        using (WebClient client = new WebClient()) {
-            string json = client.DownloadString(url);
-            dynamic result = JsonConvert.DeserializeObject(json);
-
+        using (HttpClient client = new HttpClient()) {
+            var json = client.GetStringAsync(url).Result;
+            if (json == null) {
+                Console.WriteLine("Error getting data");
+                return;
+            }
+            dynamic result = JsonConvert.DeserializeObject(json) ?? throw new ArgumentException("dupa");
+            if (result?.response?.games == null)
+            {
+                Console.WriteLine("Error getting games data");
+                return;
+            }
             // Print the names of the player's games
             foreach (var game in result.response.games) {
-                Console.WriteLine(game.name);
+                Console.WriteLine((string)Convert.ToString(game.name));
             }
         }
     }
