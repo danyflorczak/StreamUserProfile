69bd05b3cb2017ae3748e923711d03d5fbb55d2f
Daniel Florczak <danyflorczak@gmail.com>
2023-01-25 21:12:37 +0100

    changes to windows forms app

--- Program.cs
+++ Program.cs
@@ -1,75 +1,210 @@
 ﻿using System;
 using System.Net.Http;
 using Newtonsoft.Json;
+using System.Xml.Serialization;
+using System.IO;
+using System.Threading.Tasks;
 
-class SteamAPI {
-    static void Main(string[] args) {
-        string steamId = "76561198209273776";
-        string apiKey = "EF5C8E2B08A10F7FF30BFBC4A595C2BB";
-        string url = $"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={apiKey}&include_appinfo=true&steamid={steamId}&format=json";
-        UserProfile profile = new UserProfile();
-        profile.SteamId = steamId;
-        profile.Games = new List<Game>();
+namespace SteamAPI
+{
+    class SteamAPI
+    {
+        static async Task Main(string[] args)
+        {
+            string steamId = "76561198057073414";
+            string apiKey = "10CE6174A0F180746BA156D8A9B84AF4";
+            UserProfile profile = await CreateAndFillUserProfile(steamId, apiKey);
 
-
-        using (HttpClient client = new HttpClient()) {
-            var json = client.GetStringAsync(url).Result;
-            if (json == null) {
-                Console.WriteLine("Error getting data");
-                return;
+            // export UserProfile object to XML file
+            XmlSerializer serializer = new XmlSerializer(typeof(gameProfile));
+            using (FileStream stream = new FileStream("UserProfile.xml", FileMode.Create))
+            {
+                serializer.Serialize(stream, profile);
             }
-            dynamic result = JsonConvert.DeserializeObject(json) ?? throw new ArgumentException("dupa");
+
+            // saving to json file
+            var tempjson = JsonConvert.SerializeObject(profile);
+            File.WriteAllText("dane.json", tempjson);
+        }
+
+        private static async Task<UserProfile> CreateAndFillUserProfile(string steamId, string apiKey)
+        {
+            string url = $"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={apiKey}&include_appinfo=true&steamid={steamId}&format=json";
+            UserProfile profile = new UserProfile();
+            profile.SteamId = steamId;
+            profile.UserName = GetPersonalDetails();
+            profile.CountryCode = GetCountryCode();
+            profile.Games = new List<Game>();
+            profile.GamesCount = 0;
+
+            using (HttpClient client = new HttpClient())
+            {
+                var json = await client.GetStringAsync(url);
+                if (json == null)
+                {
+                    Console.WriteLine("Error gettingdata");
+return profile;
+}
+            dynamic result = JsonConvert.DeserializeObject(json) ?? throw new ArgumentException("Błąd");
+            if (result?(json == null)
+{
+MessageBox.Show("Error getting data");
+return profile;
+}
+            dynamic result = JsonConvert.DeserializeObject(json) ?? throw new ArgumentException("Błąd");
             if (result?.response?.games == null)
             {
-                Console.WriteLine("Error getting games data");
-                return;
+                MessageBox.Show("Error getting games data");
+                return profile;
             }
 
-        // Narazie to nie dziala bo get game details jest jakies zjebane
-        //     foreach (var game in result.response.games) {
-        //     Game gameProfile = new Game();
-        //     gameProfile.Name = game.name;
-        //     gameProfile.Playtime = game.playtime_forever;
-        //     gameProfile.Genre = GetGameDetails(game.appid);
-        //     profile.Games.Add(gameProfile);
-        // }
+            profile.GamesCount = result.response.game_count;
 
-            foreach (var game in result.response.games) {
-                Console.WriteLine((string)Convert.ToString("Game ID: " + game.appid));
-                Console.WriteLine((string)Convert.ToString("Game: " + game.name));
-                Console.WriteLine((string)Convert.ToString("Minutes in Game: " + game.playtime_forever));
-                Console.WriteLine("----------------------------------------------------------");
+            // adding games to the user's profile
+            foreach (var game in result.response.games)
+            {
+                Game gameProfile = new Game();
+                gameProfile.Name = game.name;
+                gameProfile.Playtime = game.playtime_forever;
+                gameProfile.Developer = GetGameDetails((string)game.appid);
+                gameProfile.Genres = GetGameGenres((string)game.appid);
+                gameProfile.isFree = GetGameIsFree((string)game.appid);
+                profile.Games.Add(gameProfile);
+            }
+
+            return profile;
+        }
+    }
+
+    private string GetGameDetails(string appID)
+    {
+        string gameDetailsUrl = $"http://store.steampowered.com/api/appdetails?appids={appID}";
+            using (HttpClient client = new HttpClient())
+            {
+                var json = client.GetStringAsync(gameDetailsUrl).Result;
+                if (json == null)
+                {
+                    return "Error GetGameDetails";
+                }
+                dynamic result = JsonConvert.DeserializeObject(json) ?? throw new ArgumentException("Błąd");
+                if (result[$"{appID}"]["data"] == null)
+                {
+                    return "empty_field";
+                }
+
+                return result[$"{appID}"]["data"]["developers"][0];
             }
         }
     }
 
-    private static string GetGameDetails(int appId) {
-    string gameDetailsUrl = $"http://store.steampowered.com/api/appdetails?appids={appId}";
-    using (HttpClient client = new HttpClient()) {
-        var json = client.GetStringAsync(gameDetailsUrl).Result;
-        if (json == null) {
-            return "Error getting data";
-        }
-        dynamic result = JsonConvert.DeserializeObject(json) ?? throw new ArgumentException("dupa");
-        if (result?.response?.genres == null)
+    private string GetGameGenres(string appID)
+    {
+         string gameDetailsUrl = $"http://store.steampowered.com/api/appdetails?appids={appID}";
+            using (HttpClient client = new HttpClient())
+            {
+                var json = client.GetStringAsync(gameDetailsUrl).Result;
+                if (json == null)
+                {
+                    return "Error GetGameGenres";
+                }
+                dynamic result = JsonConvert.DeserializeObject(json) ?? throw new ArgumentException("Błąd");
+                if (result[$"{appID}"]["data"] == null)
+                {
+                    return "empty_field";
+                }
+
+                return result[$"{appID}"]["data"]["genres"][0]["description"];
+            }
+    }
+
+    private string GetGameIsFree(string appID)
+    {
+        string gameDetailsUrl = $"http://store.steampowered.com/api/appdetails?appids={appID}";
+            using (HttpClient client = new HttpClient())
+            {
+                var json = client.GetStringAsync(gameDetailsUrl).Result;
+                if (json == null)
+                {
+                    return "Error GetGameIsFree";
+                }
+                dynamic result = JsonConvert.DeserializeObject(json) ?? throw new ArgumentException("Błąd");
+                if (result[$"{appID}"]["data"] == null)
+                {
+                    return "empty_field";
+
+                }
+
+                if(result[$"{appID}"]["data"]["is_free"] == false)
+                {
+                    return "Paid";
+                }
+                else if(result[$"{appID}"]["data"]["is_free"] == true)
+                {
+                    return "Free";
+                }
+                else
+                {
+                    return "not_info";
+                }
+            }
+    }
+
+    private string GetPersonalDetails()
+    {
+        string steamId = "76561198057073414";
+            string apiKey = "10CE6174A0F180746BA156D8A9B84AF4";
+            string personalDetailsUrl = $"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={apiKey}&steamids={steamId}";
+            
+            using (HttpClient client = new HttpClient())
+            {
+                var json = client.GetStringAsync(personalDetailsUrl).Result;
+                if (json == null)
+                {
+                    return "Error GetPersonalDetails";
+                }
+                dynamic result = JsonConvert.DeserializeObject(json) ?? throw new ArgumentException("Błąd");
+                if (result.response.players[0].personaname == null)
+                {
+                    return "empty_field";
+                }
+
+                return result.response.players[0].personaname;
+            }
+    }
+
+    private string GetCountryCode()
+    {
+        string steamId = "76561198057073414";
+        string apiKey = "10CE6174A0F180746BA156D8A9B84AF4";
+        string personalDetailsUrl = $"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={apiKey}&steamids={steamId}";
+        
+        using (HttpClient client = new HttpClient())
         {
-            return "Error getting game details data";
+            var json = client.GetStringAsync(personalDetailsUrl).Result;
+            if (json == null)
+            {
+                return "Error GetCountryCode";
+            }
+            dynamic result = JsonConvert.DeserializeObject(json) ?? throw new ArgumentException("Błąd");
+            if (result.response.players[0].loccountrycode == null)
+            {
+                return "empty_field";
+            }
+
+            return result.response.players[0].loccountrycode;
         }
-        return result.response.genres[0].description;
+    }
+
+    private void exportXMLButton_Click(object sender, EventArgs e)
+    {
+        // export UserProfile object to XML file
+        XmlSerializer serializer = new XmlSerializer(typeof(UserProfile));
+        using (FileStream stream = new FileStream("UserProfile.xml", FileMode.Create))
+        {
+            serializer.Serialize(stream, _profile);
+        }
+        MessageBox.Show("XML file created!");
     }
 }
 
-    class UserProfile {
-    public string? SteamId { get; set; }
-    public List<Game>? Games { get; set; }
-    public int TotalPlaytime { get; set; }
-    }
 
-    class Game {
-    public string? Name { get; set; }
-    public int Playtime { get; set; }
-    public int RequiredAge { get; set; }
-    public string? Genre {get;set;}
-    }
-
-}
